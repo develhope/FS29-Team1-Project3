041bd514e5e3e0b9de28f4708978e26917a125a1
Francesco Simeone <devnoxis89@gmail.com>
2024-07-13 08:42:24 +0200

    add function tabs & fix some issue

--- file_progetto/app.js
+++ file_progetto/app.js
@@ -23,10 +23,7 @@ buttonEconomy[1].addEventListener("click", () => {
   ulRegione.classList.remove("ul_show");
 });
 
-localStorage.setItem(
-  "check-dropbox",
-  '<img src="./assest/icons/Icona conferma ul.svg" class="ceck">'
-);
+localStorage.setItem("check-dropbox", '<img src="./assest/icons/Icona conferma ul.svg" class="ceck">');
 for (let i = 0; i < arrayLiRegione.length; i++) {
   arrayLiRegione[i].addEventListener("click", () => {
     arrayLiRegione[i].innerHTML += localStorage.getItem("check-dropbox");
@@ -41,4 +38,35 @@ ulRegione.addEventListener(
 );
 buttonEconomy[1].addEventListener("blur", () => {
   ul_regioni.classList.remove("ul_show");
+});
+
+/*  -----------------------------------------------------------------------------------------------
+  Tabs
+--------------------------------------------------------------------------------------------------- */
+
+const tabsCardsImg = document.querySelectorAll(".tabs__cards > img");
+const tabsBtn = document.querySelectorAll(".tabs__btn");
+const textRed = document.querySelectorAll(".text-red");
+
+/* ----  Immagini Randomiche per ogni cards ----*/
+async function getImg(element) {
+  const imgResponse = await fetch("https://api.thecatapi.com/v1/images/search");
+  const imgPexel = await imgResponse.json();
+  element.src = imgPexel[0].url;
+}
+
+tabsCardsImg.forEach((element) => {
+  getImg(element);
+});
+
+/* ---- Click On tabs button ---*/
+tabsBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    tabsCardsImg.forEach((element) => {
+      getImg(element);
+      textRed.forEach((title) => {
+        title.innerText = button.innerText;
+      });
+    });
+  });
 });
