39c4532fd1a49421de4d853b47c190939b695378
Francesco Simeone <devnoxis89@gmail.com>
2024-07-23 08:26:45 +0200

    add scroll buttons on tabs section

--- file_progetto/app.js
+++ file_progetto/app.js
@@ -116,6 +116,18 @@ tabsBtn.forEach((button) => {
   });
 });
 
+/*--- Arrow tabs ---*/
+const arrowLeft = document.querySelector(".tabs__arrow__left");
+const arrowRight = document.querySelector(".tabs__arrow__right");
+const container = document.querySelector(".tabs__container");
+
+arrowLeft.addEventListener("click", () => {
+  container.scrollLeft -= 250;
+});
+arrowRight.addEventListener("click", () => {
+  container.scrollLeft += 250;
+});
+
 // Footer
 function addCheck() {
   // for (let i = 0; i < arrayLiRegione.length; i++) {
